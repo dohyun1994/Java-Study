f4bd24ea8815ec98ce394fc31598d057d7d880e2
LAPTOP-2VITFJHM\Hyun <hiyaya94@gmail.com>
2021-11-14 21:17:28 +0900

    modified Ex7_4

---
+++ ch07/src/example/TVCREx7_4.java
@@ -0,0 +1,55 @@
+package example;
+
+class TV {
+	
+	boolean power;		// 전원상태(on/off)
+	int channel;		// 채널
+	
+	void power() {
+		power = !power;
+	}
+	
+	void channelUp() {
+		++channel;
+	}
+	
+	void channelDown() {
+		--channel;
+	}
+}
+
+class VCR {
+	
+	boolean power;		// 전원상태(on/off)
+	int counter = 0;
+	
+	void power() {}
+	void play() {}
+	void stop() {}
+	void rew() {}
+	void ff() {}
+}
+
+class TVCREx7_4 extends TV {
+
+		VCR vcr = new VCR();
+		
+		void play() {
+			vcr.play();
+		}
+		
+		void stop() {
+			vcr.stop();
+		}
+		
+		void rew() {
+			vcr.rew();
+		}
+		
+		void ff() {
+			vcr.ff();
+		}
+		
+}
+
+
