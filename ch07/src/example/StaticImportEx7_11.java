8bdf4df7316061dbdec7b59616cf7d16863e5b37
LAPTOP-2VITFJHM\Hyun <hiyaya94@gmail.com>
2021-11-20 20:40:08 +0900

    add new file

---
+++ ch07/src/example/StaticImportEx7_11.java
@@ -0,0 +1,16 @@
+package example;
+
+import static java.lang.System.out;
+import static java.lang.Math.*;
+
+public class StaticImportEx7_11 {
+
+	public static void main(String[] args) {
+		
+		out.println(random());
+		
+		out.println("Math.PI :" + PI);
+
+	}
+
+}
